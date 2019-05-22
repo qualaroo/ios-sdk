fb3f98d5c2d787fe829654debc124ed32eb205ab
Marcin Robaczyński <scanarch@gmail.com>
2019-05-22 22:07:09 +0200

    Add naive bundle verification process in order to help with debugging

--- Qualaroo/Extensions/Bundle.swift
+++ Qualaroo/Extensions/Bundle.swift
@@ -16,8 +16,27 @@ extension Bundle {
   /// - Returns: Bundle used internally by Qualaroo Framework.
   static func qualaroo() -> Bundle? {
     let bundle = Bundle(for: Qualaroo.self)
-    guard let url = bundle.resourceURL else { return nil }
+    if (containsQualarooResources(bundle)) {
+        Qualaroo.log("Using Qualaroo.self bundle")
+        return bundle
+    }
+    
+    guard let url = bundle.resourceURL else {
+        Qualaroo.log("Qualaroo.self bundle resourceUrl is nil.")
+        return nil
+    }
+    
     let qualarooBundleUrl = url.appendingPathComponent("Qualaroo.bundle")
-    return Bundle(url: qualarooBundleUrl)
+    let qualarooBundle = Bundle(url: qualarooBundleUrl)
+    if (!containsQualarooResources(qualarooBundle)) {
+        Qualaroo.log("Qualaroo.bundle does not exist or does not contain Qualaroo resources. Main path: \(url)")
+    }
+    return qualarooBundle
   }
+    
+    private static func containsQualarooResources(_ bundle: Bundle?) -> Bool {
+        guard let bundle = bundle else { return false }
+        let path = bundle.path(forResource: "AnswerBinaryView", ofType: "nib")
+        return path != nil
+    }
 }
