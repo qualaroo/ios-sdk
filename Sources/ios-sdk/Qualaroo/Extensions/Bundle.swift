309a294f6bbde7fbec5d02a1c6bb03da097d73cb
Dawid Zubrowski <dawid.zubrowski@vattenfall.com>
2022-09-28 15:39:35 +0200

    trying to run this

--- Sources/ios-sdk/Qualaroo/Extensions/Bundle.swift
+++ Sources/ios-sdk/Qualaroo/Extensions/Bundle.swift
@@ -14,61 +14,33 @@ import Foundation
 
 private class BundleFinder {}
 
-extension Foundation.Bundle {
-    /// Returns the resource bundle associated with the current Swift module.
-    static func qualaroo() -> Bundle? {
-        let bundleName = "Qualaroo"
+extension Bundle {
+  /// Convenience way to get Qualaroo bundle.
+  ///
+  /// - Returns: Bundle used internally by Qualaroo Framework.
+  static func qualaroo() -> Bundle? {
+    let bundle = Bundle(for: Qualaroo.self)
+    if (containsQualarooResources(bundle)) {
+        Qualaroo.log("Using Qualaroo.self bundle")
+        return bundle
+    }
 
-        let candidates: [URL?] = [
-            // Bundle should be present here when the package is linked into an App.
-            Bundle.main.resourceURL,
+    guard let url = bundle.resourceURL else {
+        Qualaroo.log("Qualaroo.self bundle resourceUrl is nil.")
+        return nil
+    }
 
-            // Bundle should be present here when the package is linked into a framework.
-            Bundle(for: BundleFinder.self).resourceURL,
+    let qualarooBundleUrl = url.appendingPathComponent("Qualaroo.bundle")
+    let qualarooBundle = Bundle(url: qualarooBundleUrl)
+    if (!containsQualarooResources(qualarooBundle)) {
+        Qualaroo.log("Qualaroo.bundle does not exist or does not contain Qualaroo resources. Main path: \(url)")
+    }
+    return qualarooBundle
+  }
 
-            // For command-line tools.
-            Bundle.main.bundleURL,
-
-            Bundle(for: BundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent(),
-        ]
-
-        for candidate in candidates {
-            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
-            if let bundle = bundlePath.flatMap(Bundle.init(url: bundlePath!)) {
-                return bundle
-            }
-        }
-        fatalError("unable to find bundle named \(bundleName)")
+    private static func containsQualarooResources(_ bundle: Bundle?) -> Bool {
+        guard let bundle = bundle else { return false }
+        let path = bundle.path(forResource: "AnswerBinaryView", ofType: "nib")
+        return path != nil
     }
 }
-
-//extension Bundle {
-//  /// Convenience way to get Qualaroo bundle.
-//  ///
-//  /// - Returns: Bundle used internally by Qualaroo Framework.
-//  static func qualaroo() -> Bundle? {
-//    let bundle = Bundle(for: Qualaroo.self)
-//    if (containsQualarooResources(bundle)) {
-//        Qualaroo.log("Using Qualaroo.self bundle")
-//        return bundle
-//    }
-//
-//    guard let url = bundle.resourceURL else {
-//        Qualaroo.log("Qualaroo.self bundle resourceUrl is nil.")
-//        return nil
-//    }
-//
-//    let qualarooBundleUrl = url.appendingPathComponent("Qualaroo.bundle")
-//    let qualarooBundle = Bundle(url: qualarooBundleUrl)
-//    if (!containsQualarooResources(qualarooBundle)) {
-//        Qualaroo.log("Qualaroo.bundle does not exist or does not contain Qualaroo resources. Main path: \(url)")
-//    }
-//    return qualarooBundle
-//  }
-//
-//    private static func containsQualarooResources(_ bundle: Bundle?) -> Bool {
-//        guard let bundle = bundle else { return false }
-//        let path = bundle.path(forResource: "AnswerBinaryView", ofType: "nib")
-//        return path != nil
-//    }
-//}
