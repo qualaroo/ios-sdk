708a24724ef7e3029819b05eadf9edec5391104f
Dawid Zubrowski <dawid.zubrowski@vattenfall.com>
2022-09-28 15:51:51 +0200

    trying to run this

--- Sources/ios-sdk/Qualaroo/Extensions/Bundle.swift
+++ Sources/ios-sdk/Qualaroo/Extensions/Bundle.swift
@@ -19,30 +19,28 @@ extension Bundle {
   ///
   /// - Returns: Bundle used internally by Qualaroo Framework.
     static func qualaroo() -> Bundle? {
-        let bundle: Bundle = .module
-        return bundle
+        let bundle = Bundle(for: Qualaroo.self)
+        if (containsQualarooResources(bundle)) {
+            Qualaroo.log("Using Qualaroo.self bundle")
+            return bundle
+        }
+        
+        guard let url = bundle.resourceURL else {
+            Qualaroo.log("Qualaroo.self bundle resourceUrl is nil.")
+            return nil
+        }
+        
+        let qualarooBundleUrl = url.appendingPathComponent("Qualaroo.bundle")
+        let qualarooBundle = Bundle(url: qualarooBundleUrl)
+        if (!containsQualarooResources(qualarooBundle)) {
+            Qualaroo.log("Qualaroo.bundle does not exist or does not contain Qualaroo resources. Main path: \(url)")
+        }
+        return qualarooBundle
     }
-//    if (containsQualarooResources(bundle)) {
-//        Qualaroo.log("Using Qualaroo.self bundle")
-//        return bundle
-//    }
-//
-//    guard let url = bundle.resourceURL else {
-//        Qualaroo.log("Qualaroo.self bundle resourceUrl is nil.")
-//        return nil
-//    }
-//
-//    let qualarooBundleUrl = url.appendingPathComponent("Qualaroo.bundle")
-//    let qualarooBundle = Bundle(url: qualarooBundleUrl)
-//    if (!containsQualarooResources(qualarooBundle)) {
-//        Qualaroo.log("Qualaroo.bundle does not exist or does not contain Qualaroo resources. Main path: \(url)")
-//    }
-//    return qualarooBundle
-//  }
-//
-//    private static func containsQualarooResources(_ bundle: Bundle?) -> Bool {
-//        guard let bundle = bundle else { return false }
-//        let path = bundle.path(forResource: "AnswerBinaryView", ofType: "nib")
-//        return path != nil
-//    }
+    
+    private static func containsQualarooResources(_ bundle: Bundle?) -> Bool {
+        guard let bundle = bundle else { return false }
+        let path = bundle.path(forResource: "AnswerBinaryView", ofType: "nib")
+        return path != nil
+    }
 }
