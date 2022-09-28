6abc8f500004f63110ca796cc01508ed52d93f64
Dawid Zubrowski <dawid.zubrowski@vattenfall.com>
2022-09-28 14:40:18 +0200

    add QualarooStoryboard to resources

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ios-sdk",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("QualarooStoryboard.storyboard")
+            ]),
         .testTarget(
             name: "ios-sdkTests",
             dependencies: ["ios-sdk"]),
